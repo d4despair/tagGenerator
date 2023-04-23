256064f05704b75ac639f2e6e1683176a3dbf61b
d4despair <diocai2020@163.com>
2023-04-23 14:46:02 +0800

    20230423 优化备份

--- taggen/udt/reader/datablock.py
+++ taggen/udt/reader/datablock.py
@@ -8,8 +8,10 @@ def read_db_from_txt(filename: str):
     print('正在处理：{}'.format(filename))
     f = open(filename, 'r', encoding='utf-8')
     lines = f.readlines()
-    db_list = []
-    st_list = []
+    db_list = []  # DB块列表，由许多DB块组成
+    db = []  # DB块
+    db_title = ''  # DB块标题
+    st_list = []  # 结构列表，由许多结构组成
 
     # 遍历文件内容
     while True:
@@ -20,154 +22,126 @@ def read_db_from_txt(filename: str):
 
         # 查找DB块
         if res := re.search(r'DATA_BLOCK "(.*)"', temp_line):
-            title = res[1]
+            db_title = res[1]
             db = []
-            db_line = [title]
-            # print(f'正在处理: {title} ...')
 
         # 查找变量
-        if res := re.search(r'(.*) : "(.*)";(.*)', temp_line):
-            # 变量名
-            data_name = res.group(1)
-            if res_name := re.search(r'(.*){(.*)}', data_name):
-                db_line.append(res_name.group(1).strip())
-            else:
-                db_line.append(data_name.strip())
-            # 变量类型
-            data_type = res.group(2).strip()
-            db_line.append(data_type)
-            # 变量注释
-            data_remark = res.group(3)
-            if res_remark := re.search(r'//(.*)', data_remark):
-                db_line.append(res_remark.group(1).strip())
-            else:
-                db_line.append('')
-            # print(db_line)
-            db.append(db_line)
-            db_line = [title]
+        if db_data := parse_data(temp_line, db_title):
+            db.append(db_data)
 
         # 查找结构
-        if res := re.search(r'(.*): (Struct.*)', temp_line):
-            st_name = res.group(1)
-            if res_name := re.search(r'(.*){(.*)}', st_name):
-                db_line.append(res_name.group(1).strip())
-            else:
-                db_line.append(st_name.strip())
 
-            st_type = f'{title}_{db_line[-1]}_Struct'.upper()
-            db_line.append(st_type)
-
-            st_remark = res.group(2)
-            if res_remark := re.search(r'//(.*)', st_remark):
-                db_line.append(res_remark.group(1).strip())
-            else:
-                db_line.append('')
-
-            db.append(db_line)
-            db_line = [title]
-            print(res.group(0))
+        if db_struct := parse_struct(temp_line, db_title, lines, st_list):
+            db.append(db_struct[0])  # DB行
+            st_list.append(db_struct[1])  # 结构
 
         # 结束DB块
         if re.search(r'END_DATA_BLOCK', temp_line):
             if len(db) > 0:
                 db_list.append(db)
             else:
-                print(f"无法添加，DB名称：{title} 数据个数：0")
-                # raise Exception(f"DB名称：{title} 数据个数：0")
-            db = []
-    # print(db_list)
+                print(f"无法添加，DB名称：{db_title} 数据个数：0")
 
+            # 创建新DB
+            db = []
+
+    # print(db_list)
     [print(f'DB名称：\'{db[0][0]}\' 数据个数：{len(db)}') for db in db_list]
     print(f'处理完成，总共 {len(db_list)} 个DB')
+    [print(f'结构名称：\'{st[0][0]}\' 数据个数：{len(st)}') for st in st_list]
+    print(f'处理完成，总共 {len(st_list)} 个结构')
 
-    return db_list
+    return db_list, st_list
+
+
+# 提取注释
+def get_remark(string: str):
+    if res := re.search(r'//(.*)', string):
+        return res.group(1).strip()
+    else:
+        return ''
+
+
+# 提取变量名
+def get_name(string: str):
+    if res := re.search(r'(.*){(.*)}', string):
+        return res.group(1).strip()
+    else:
+        return string.strip()
+
+
+# 解析结构，返回（结构变量，结构列表）
+def parse_struct(line: str, title: str, lines: list, st_list: list):
+    parent_data = [title]
+    self_st = []
+    child_st = []
+    child_list = None
+    if res := re.search(r'(.*): (Struct.*)', line):
+        # 结构名
+        st_name = get_name(res.group(1))
+        parent_data.append(st_name)
+
+        # 结构类型
+        st_type = f'{title}.{parent_data[-1]}'
+        parent_data.append(st_type)
+
+        # 结构注释
+        st_remark = res.group(2).strip()
+        parent_data.append(get_remark(st_remark))
+    else:
+        return
+
+    while not re.search(r'end_struct', line, re.I):
+        try:
+            line = lines.pop(0)
+        except IndexError:
+            break
+
+        # 结构里的数据
+        if st_data := parse_data(line, st_type):
+            self_st.append(st_data)
+
+        # 嵌套结构
+        if child_struct := parse_struct(line, st_type, lines, st_list):
+            child_st.append(child_struct[0])  # 结构添加一个数据
+            self_st.extend(child_st)  # 结构添加所有子数据
+            child_list = child_struct[1].copy()
+            if child_list:
+                st_list.insert(0, child_list)
+            print(child_list)
+    return parent_data, self_st, child_list
+
+
+def parse_data(line: str, parent_title: str):
+    # 查找变量
+    db_data = [parent_title]
+    if res := re.search(r'(.*) : (.*);(.*)', line):
+        # 变量名
+        data_name_raw = res.group(1).strip()
+        db_data.append(get_name(data_name_raw))
+
+        # 变量类型
+        data_type_raw = res.group(2).strip()
+        db_data.append(data_type_raw)
+
+        # 变量注释
+        data_remark_raw = res.group(3).strip()
+        db_data.append(get_remark(data_remark_raw))
+        return db_data
+    else:
+        return
 
 
 def test():
-    filepath = 'D:\\工作资料\\10-宝尔康资料\\tia_db\\'
-    filename = 'sg'
-    db_list = read_db_from_txt(f'{filepath}{filename}.db')
+    # filepath = 'D:\\工作资料\\10-宝尔康资料\\tia_db\\'
+    filepath = 'D:\\工作资料\\11-PYTHON测试\\'
+    filename = 'S_HP'
+    db_list, st_list = read_db_from_txt(f'{filepath}{filename}.db')
     from taggen.udt.writer.datablock import write_db_to_excel
-    write_db_to_excel(f'{filepath}{filename}.xlsx', db_list)
+    from taggen.udt.reader.offset import add_offset
+    [add_offset(st) for st in st_list]
+    write_db_to_excel(f'{filepath}{filename}.xlsx', db_list, st_list)
 
 
 if __name__ == '__main__':
     test()
-
-#
-# def get_udt_from_txt(filename: str) -> UDT | None:
-#     if filename.endswith('.txt'):
-#         print('正在处理: {}'.format(filename))
-#         f = open(filename, 'r', encoding='utf-8')
-#         lines = f.readlines()
-#
-#         while True:
-#             temp_line = lines.pop(0)
-#             if 'DATA_BLOCK' in temp_line:
-#                 print('目前无法处理DB文件')
-#                 return
-#             if 'TYPE' in temp_line:
-#                 break
-#
-#         new_udt = UDT()
-#         struct = []
-#         # udt 类型
-#         new_udt.udt_type = temp_line[temp_line.find('"') + 1:temp_line.rfind('"')].strip()
-#
-#         udt_fd = list(new_udt.__dict__)
-#         # print(udt_fd)
-#
-#         # udt 作者 描述 版本号
-#         for i in range(1, 4):
-#             temp_line = lines.pop(0)
-#             # print('udt_fd[{}]: , new_udt[{}]'.format(i, udt_fd[i])
-#             new_udt[udt_fd[i]] = temp_line[temp_line.find(':') + 1:temp_line.rfind('\n')].strip().lower()
-#
-#         # 读变量
-#         offset = 0
-#         prev_type = ''
-#         while len(lines) > 0:
-#             temp_line = lines.pop(0)
-#             if 'END_TYPE' in temp_line:
-#                 break
-#             elif ':' not in temp_line:
-#                 continue
-#
-#             new_tag = _txt_read_tag(temp_line)
-#
-#             if tagtype.is_bool(prev_type):
-#                 if tagtype.is_bool(new_tag.type):
-#                     if offset * 10 % 10 > 7:
-#                         offset = math.floor(offset) + 1
-#                 else:
-#                     offset = math.floor(offset)
-#                     if offset % 2 == 1:
-#                         offset += 1
-#                     else:
-#                         offset += 2
-#             if tagtype.is_bool(new_tag.type):
-#                 new_tag.offset = '%.1f' % offset
-#             else:
-#                 new_tag.offset = '%d' % offset
-#
-#             offset += tagtype.type_length(new_tag.type)
-#             prev_type = new_tag.type
-#             struct.append(new_tag)
-#             new_udt.struct = struct
-#
-#         # udt 长度
-#         if tagtype.is_bool(prev_type):
-#             offset = math.floor(offset)
-#             if offset % 2 == 1:
-#                 offset += 1
-#             else:
-#                 offset += 2
-#         new_udt.length = offset
-#
-#         f.close()
-#         print('处理完毕')
-#         return new_udt
-#     else:
-#         print('无法处理: {}'.format(filename))
-#
-#     return
--- taggen/udt/writer/datablock.py
+++ taggen/udt/writer/datablock.py
@@ -3,12 +3,27 @@
 
 from openpyxl import Workbook
 
+DB_HEADER = ['数据块', '数据名称', '数据类型', '注释']
+ST_HEADER = ['所属结构', '数据名称', '数据类型', '注释']
 
-def write_db_to_excel(filename: str, db_list):
+
+def write_db_to_excel(filename: str, db_list: list, st_list: list = None):
     wb = Workbook()
+
     ws = wb.active
+    ws.append(DB_HEADER)
     for db in db_list:
         for db_line in db:
             ws.append(db_line)
+    ws.title = 'datablock'
+
+    ws = wb.create_sheet('struct', 1)
+    ws.append(ST_HEADER)
+    if st_list:
+        st_list.sort(reverse=1)
+        for st in st_list:
+            for st_line in st:
+                ws.append(st_line)
     wb.save(filename)
+    wb.close()
     print(f'文件保存至：{filename}')
